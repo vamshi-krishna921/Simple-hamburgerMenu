2b5e4f8cf3c8233260cc64c31b2404c0b87f162d
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-04-19 20:31:48 +0530

    fully completed

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ let cross = document.querySelector(".cross");
 let header = document.querySelector(".header");
 let hamburgerMenu = document.querySelector(".ham-menu");
 let nav = document.querySelector("nav");
-hamburgerMenu.addEventListener("click", () => {
+hamburgerMenu.addEventListener("click", (e) => {
   hamburgerMenu.style.display = "none";
   header.classList.add("menu-open");
   cross.style.display = "block";
+  e.stopPropagation();
 });
-cross.addEventListener("click", () => {
+function close() {
   cross.style.display = "none";
   hamburgerMenu.style.display = "block";
   header.classList.remove("menu-open");
+}
+cross.addEventListener("click", (e) => {
+  close();
+});
+document.addEventListener("click", (e) => {
+  if (header.classList.contains("menu-open")) {
+    close();
+  }
+});
+nav.addEventListener("click", (e) => {
+  e.stopPropagation(); 
 });
